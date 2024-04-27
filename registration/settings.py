7afb4e5a6a1974c5cf97072226ea78dd57aac2a5
sufyandhillon <sufyandhillon72@gmail.com>
2024-04-28 01:24:45 +0500

    update setting.py

--- registration/settings.py
+++ registration/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure---1z_j%j$ky4e8-b8%bf21#*fkeh4)2tuutbzem%r0x+9a(r+z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'registration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [ BASE_DIR/ 'templates' ] ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
